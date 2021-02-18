c534435f2217653fb60efd176e931b87deaf7b2b
Francesco Cini <francesco.cini@gruppometa.it>
2021-02-18 08:57:51 +0100

    modifica su upload

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -3,23 +3,8 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { User } from "../../model/user.model";
 import { ApiService } from "../../service/api.service";
 import { Toaster } from 'ngx-toast-notifications';
-import { FileUploader, FileLikeObject } from 'ng2-file-upload';
-
-function readBase64(file): Promise<any> {
-  var reader  = new FileReader();
-  var future = new Promise((resolve, reject) => {
-    reader.addEventListener("load", function () {
-      resolve(reader.result);
-    }, false);
-
-    reader.addEventListener("error", function (event) {
-      reject(event);
-    }, false);
-
-    reader.readAsDataURL(file);
-  });
-  return future;
-}
+import { FileUploader, FileLikeObject, FileItem, ParsedResponseHeaders } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'user',
@@ -31,12 +16,12 @@ export class UserComponent implements OnInit {
 
   user: User;
   public uploader: FileUploader = new FileUploader({
-    url: 'http://localhost:3000/api/upload',
+    url: environment.apiURL + 'upload',
     disableMultipart : false,
     autoUpload: true,
     method: 'post',
     itemAlias: 'attachment',
-    allowedFileType: ['image', 'pdf'],
+    allowedFileType: ['image', 'pdf','doc','xls','compress'],
     authToken: 'Bearer '+localStorage.getItem('token'),
     });
 
@@ -47,12 +32,37 @@ export class UserComponent implements OnInit {
 
     ngOnInit() {
       this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+      this.uploader.onErrorItem = (item, response, status, headers) => this.onErrorItem(item, response, status, headers);
+      this.uploader.onSuccessItem = (item, response, status, headers) => this.onSuccessItem(item, response, status, headers);
+  
        const id = parseInt(this.route.snapshot.paramMap.get('id'))
        this.apiService.getUserById(id)
          .subscribe(data => {
            this.user = data;
          });
    }
+
+  onSuccessItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
+    let data = JSON.parse(response); 
+    console.log('item uploaded',data);
+    this.toaster.open({
+      text: 'Item uploaded',
+      position: 'top-right',
+      duration: 3000,
+      type: 'success'
+    });
+  }
+
+  onErrorItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
+    let error = JSON.parse(response); 
+    console.log('error on item upload',error);
+    this.toaster.open({
+      text: 'Upload error',
+      position: 'top-right',
+      duration: 3000,
+      type: 'warning'
+    });
+  }
 
    public onFileSelected(event: EventEmitter<File[]>) {
     const file: File = event[0];
